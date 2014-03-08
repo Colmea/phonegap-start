95ed1ab7f14f51680825bf8f810799a71e1e76cd
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2014-03-08 16:04:08 +0100

    Re-add barcode scanner plugin (?)

--- www/js/app/views/Home.js
+++ www/js/app/views/Home.js
@@ -46,6 +46,17 @@ define(function (require) {
 
         scan: function(event) {
 
+            cordova.plugins.barcodeScanner.scan(
+              function (result) {
+                  alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+              }, 
+              function (error) {
+                  alert("Scanning failed: " + error);
+              }
+           );
         }
 
     });
